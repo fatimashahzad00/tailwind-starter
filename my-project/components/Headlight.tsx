8c752ab6eba94629a552ebd34aa1ea93f5a23117
Fatima Shahzad <107702644+fatimashahzad00@users.noreply.github.com>
2023-03-16 21:10:39 +0500

    Delete my-project directory

--- my-project/components/Headlight.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function CarHeadLight () {
-    return <div className='bg-yellow-300 h-5 w-5'>CH</div>
-}
-
-
-export default CarHeadLight;
--- my-project/components/Seat.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Seat () {
- return   <div className="bg-amber-600 h-8 w-8"></div>
-}
-
-export default Seat;
